25cf588b99fb06aee59f12189f7503abc27320c4
rıfat yücel <yucel.rifatt@gmail.com>
2021-02-15 22:53:39 +0300

    codes added

---
+++ flood it/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace flood_it
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.PBoxRed = new System.Windows.Forms.PictureBox();
+            this.PBoxWhite = new System.Windows.Forms.PictureBox();
+            this.PBoxBlack = new System.Windows.Forms.PictureBox();
+            this.PBoxGray = new System.Windows.Forms.PictureBox();
+            this.PBoxOrange = new System.Windows.Forms.PictureBox();
+            this.PBoxYellow = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxWhite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxBlack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxGray)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxOrange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxYellow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(202, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 32);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // PBoxRed
+            // 
+            this.PBoxRed.BackColor = System.Drawing.Color.Red;
+            this.PBoxRed.Location = new System.Drawing.Point(5, 425);
+            this.PBoxRed.Name = "PBoxRed";
+            this.PBoxRed.Size = new System.Drawing.Size(73, 65);
+            this.PBoxRed.TabIndex = 1;
+            this.PBoxRed.TabStop = false;
+            this.PBoxRed.Tag = "red";
+            this.PBoxRed.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // PBoxWhite
+            // 
+            this.PBoxWhite.BackColor = System.Drawing.Color.White;
+            this.PBoxWhite.Location = new System.Drawing.Point(84, 425);
+            this.PBoxWhite.Name = "PBoxWhite";
+            this.PBoxWhite.Size = new System.Drawing.Size(73, 65);
+            this.PBoxWhite.TabIndex = 2;
+            this.PBoxWhite.TabStop = false;
+            this.PBoxWhite.Tag = "white";
+            this.PBoxWhite.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // PBoxBlack
+            // 
+            this.PBoxBlack.BackColor = System.Drawing.Color.Black;
+            this.PBoxBlack.Location = new System.Drawing.Point(163, 425);
+            this.PBoxBlack.Name = "PBoxBlack";
+            this.PBoxBlack.Size = new System.Drawing.Size(73, 65);
+            this.PBoxBlack.TabIndex = 3;
+            this.PBoxBlack.TabStop = false;
+            this.PBoxBlack.Tag = "black";
+            this.PBoxBlack.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // PBoxGray
+            // 
+            this.PBoxGray.BackColor = System.Drawing.Color.Gray;
+            this.PBoxGray.Location = new System.Drawing.Point(242, 425);
+            this.PBoxGray.Name = "PBoxGray";
+            this.PBoxGray.Size = new System.Drawing.Size(73, 65);
+            this.PBoxGray.TabIndex = 4;
+            this.PBoxGray.TabStop = false;
+            this.PBoxGray.Tag = "gray";
+            this.PBoxGray.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // PBoxOrange
+            // 
+            this.PBoxOrange.BackColor = System.Drawing.Color.Orange;
+            this.PBoxOrange.Location = new System.Drawing.Point(321, 425);
+            this.PBoxOrange.Name = "PBoxOrange";
+            this.PBoxOrange.Size = new System.Drawing.Size(73, 65);
+            this.PBoxOrange.TabIndex = 5;
+            this.PBoxOrange.TabStop = false;
+            this.PBoxOrange.Tag = "orange";
+            this.PBoxOrange.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // PBoxYellow
+            // 
+            this.PBoxYellow.BackColor = System.Drawing.Color.Yellow;
+            this.PBoxYellow.Location = new System.Drawing.Point(400, 425);
+            this.PBoxYellow.Name = "PBoxYellow";
+            this.PBoxYellow.Size = new System.Drawing.Size(73, 65);
+            this.PBoxYellow.TabIndex = 6;
+            this.PBoxYellow.TabStop = false;
+            this.PBoxYellow.Tag = "yellow";
+            this.PBoxYellow.Click += new System.EventHandler(this.PBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(416, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0/24";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(478, 502);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PBoxYellow);
+            this.Controls.Add(this.PBoxOrange);
+            this.Controls.Add(this.PBoxGray);
+            this.Controls.Add(this.PBoxBlack);
+            this.Controls.Add(this.PBoxWhite);
+            this.Controls.Add(this.PBoxRed);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flood It";
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxWhite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxBlack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxGray)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxOrange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxYellow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox PBoxRed;
+        private System.Windows.Forms.PictureBox PBoxWhite;
+        private System.Windows.Forms.PictureBox PBoxBlack;
+        private System.Windows.Forms.PictureBox PBoxGray;
+        private System.Windows.Forms.PictureBox PBoxOrange;
+        private System.Windows.Forms.PictureBox PBoxYellow;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ flood it/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flood_it
+{
+    public partial class Form1 : Form
+    {
+        Brush myBrush;
+        Graphics graphicsObject;
+        List<Color> lstGameColours = new List<Color>();
+        List<string> lstGameColourNames = new List<string>();
+
+        string colourName;
+        Random rnd = new Random();
+
+        List<List<Rectangle>> lst2dSqures = new List<List<Rectangle>>();
+
+        List<List<string>> lst2dSquareColourNames = new List<List<string>>();
+
+        List<List<bool>> lst2dIsSquareColourChanged = new List<List<bool>>();
+
+        string clickedColourName;
+        int clickCounter = 0;
+        bool gameOver;
+        public Form1()
+        {
+            InitializeComponent();
+            graphicsObject = this.CreateGraphics();
+            setColours();
+        }
+        void setColours()
+        {
+            lstGameColours.Add(Color.Red);
+            lstGameColours.Add(Color.White);
+            lstGameColours.Add(Color.Black);
+            lstGameColours.Add(Color.Gray);
+            lstGameColours.Add(Color.Yellow);
+            lstGameColours.Add(Color.Orange);
+
+            lstGameColourNames.Add("red");
+            lstGameColourNames.Add("white");
+            lstGameColourNames.Add("black");
+            lstGameColourNames.Add("gray");
+            lstGameColourNames.Add("yellow");
+            lstGameColourNames.Add("orange"); 
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            createSquares();
+
+            lst2dIsSquareColourChanged[0][0] = true;
+        }
+        void createSquares()
+        {
+            List<Rectangle> lstSquaresInRow = new List<Rectangle>();
+            List<string> lstColourNamesInRow = new List<string>();
+            List<bool> lstIsColourChangedInRow = new List<bool>();
+
+            Rectangle rect = new Rectangle();
+            rect.Height = 30;
+            rect.Width = 30;
+
+            for (int row = 0; row < 12; row++)
+            {
+                for (int column = 0; column < 12; column++)
+                {
+                    setSquareColour();
+                    rect.Location = new Point((column * 30) + 60, (row * 30) + 50);
+
+                    graphicsObject.FillRectangle(myBrush, rect);
+                    lstSquaresInRow.Add(rect);
+                    lstColourNamesInRow.Add(colourName);
+                    lstIsColourChangedInRow.Add(false);
+                }
+
+                lst2dSqures.Add(lstSquaresInRow);
+                lstSquaresInRow = new List<Rectangle>();
+
+                lst2dSquareColourNames.Add(lstColourNamesInRow);
+                lstColourNamesInRow = new List<string>();
+
+                lst2dIsSquareColourChanged.Add(lstIsColourChangedInRow);
+                lstIsColourChangedInRow = new List<bool>();
+            }
+        }
+        void setSquareColour()
+        {
+            int colourNumber = rnd.Next(0, 6);
+            myBrush = new SolidBrush(lstGameColours[colourNumber]);
+            colourName = lstGameColourNames[colourNumber];
+        }
+        private void PBox_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPBox = (PictureBox)sender;
+            clickCounter++;
+            label1.Text = clickCounter.ToString() + "/24";
+            if (!gameOver && !checkCounter())
+            {
+                clickedColourName = clickedPBox.Tag.ToString();
+                myBrush = new SolidBrush(clickedPBox.BackColor);
+                changeSquareColours();
+            }
+        }
+        private bool checkCounter()
+        {
+            bool status = false;
+            if (clickCounter > 24)
+            {
+                status = true;
+                MessageBox.Show("You Lost! :(");
+                restartGame();
+            }
+            return status;
+        }
+        void restartGame()
+        {
+            lst2dSqures.Clear();
+            lst2dSqures.TrimExcess();
+            lst2dSqures = new List<List<Rectangle>>();
+
+            lst2dSquareColourNames.Clear();
+            lst2dSquareColourNames.TrimExcess();
+            lst2dSquareColourNames = new List<List<string>>();
+
+            lst2dIsSquareColourChanged.Clear();
+            lst2dIsSquareColourChanged.TrimExcess();
+            lst2dIsSquareColourChanged = new List<List<bool>>();
+
+            clickCounter = 0;
+            gameOver = false;
+        }
+        void changeSquareColours()
+        {
+            if (!gameOver)
+            {
+                paintPreviouslyChangedRectangles();
+                int loop = 0;
+
+            start:
+                if (loop < 3)
+                {
+                    for (int row = 0; row < 12; row++)
+                    {
+                        for (int column = 0; column < 12; column++)
+                        {
+                            if (row == 0 && column >= 0)
+                            {
+                                checkFirstRow(column);
+                            }
+                            else if (row > 0 && clickedColourName == lst2dSquareColourNames[row][column])
+                            {
+                                if (controlSidesIfColourChanged(row, column))
+                                {
+                                    lst2dIsSquareColourChanged[row][column] = true;
+
+                                    graphicsObject.FillRectangle(myBrush, lst2dSqures[row][column]);
+                                    lst2dSquareColourNames[row][column] = clickedColourName;
+
+                                    checkSquareSidesForColourSameness(row, column);
+                                }
+                            }
+                        }
+                    }
+                    loop++;
+                    goto start;
+                }
+                gameControl(clickedColourName);
+            }
+        }
+        void paintPreviouslyChangedRectangles()
+        {
+            for (int row = 0; row < 12; row++)
+            {
+                for (int column = 0; column < 12; column++)
+                {
+                    if (lst2dIsSquareColourChanged[row][column])
+                    {
+                        graphicsObject.FillRectangle(myBrush, lst2dSqures[row][column]);
+                        lst2dSquareColourNames[row][column] = clickedColourName;
+                    }
+                }
+            }
+        }
+        void checkFirstRow(int column)
+        {
+            if (lst2dSquareColourNames[0][column] == clickedColourName)
+            {
+                try
+                {
+                    {
+                        if (lst2dIsSquareColourChanged[0][column - 1] || lst2dIsSquareColourChanged[1][column] || lst2dIsSquareColourChanged[0][column + 1])
+                        {
+                            lst2dIsSquareColourChanged[0][column] = true;
+                            graphicsObject.FillRectangle(myBrush, lst2dSqures[0][column]);
+                            lst2dSquareColourNames[0][column] = clickedColourName;
+                        }
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+        private bool controlSidesIfColourChanged(int satir, int sutun)
+        {
+            bool status = false;
+            try
+            {
+                if (lst2dIsSquareColourChanged[satir - 1][sutun] || lst2dIsSquareColourChanged[satir][sutun - 1] || lst2dIsSquareColourChanged[satir + 1][sutun] || lst2dIsSquareColourChanged[satir][sutun + 1])
+                {
+                    status = true;
+                }
+            }
+            catch (Exception) { }
+            return status;
+        }
+        void checkSquareSidesForColourSameness(int row, int column)
+        {
+            try
+            {
+                if (clickedColourName == lst2dSquareColourNames[row - 1][column])
+                {
+                    lst2dIsSquareColourChanged[row - 1][column] = true;
+                }
+                if (clickedColourName == lst2dSquareColourNames[row - 1][column - 1])
+                {
+                    lst2dIsSquareColourChanged[row - 1][column - 1] = true;
+                }
+                if (clickedColourName == lst2dSquareColourNames[row - 1][column + 1])
+                {
+                    lst2dIsSquareColourChanged[row - 1][column + 1] = true;
+                }
+            }
+            catch (Exception){}
+        }
+        void gameControl(string colourName)
+        {
+            int changedColourNumber = 0;
+
+            for (int row = 0; row < 12; row++)
+            {
+                for (int column = 0; column < 12; column++)
+                {
+                    if (lst2dSquareColourNames[row][column] == colourName)
+                    {
+                        changedColourNumber++;
+                    }
+                }
+            }
+
+            if (clickCounter <= 24 && changedColourNumber == 144)
+            {
+                MessageBox.Show("You Won! :)");
+                gameOver = true;
+                restartGame();
+            }
+        }
+    }
+}
+
+    
